008152960b7a8112c081969bb81c4665a269e89e
Kane Lincoln <kane@mba.local>
2023-04-13 17:54:49 +0100

    Added a second integration test while also seeding the test database ahead of time. Added the --watch flag to the 'npm run test' script.

--- tests/queries.test.js
+++ tests/queries.test.js
@@ -1,9 +1,54 @@
+const { PrismaClient } = require('@prisma/client');
 const { gql } = require('apollo-server-express');
 
 const { testServer } = require('./testServer');
 
-describe('Query: boroughsWithSpaces', () => {
-  it('returns an array of boroughs that have at least one space associated with it;', async () => {
+const prisma = new PrismaClient();
+
+describe('boroughsWithSpaces', () => {
+  beforeAll(async () => {
+    // The database must be populated in advance.
+    const borough = await prisma.borough.create({
+      data: {
+        name: 'Southwark'
+      }
+    });
+
+    await prisma.space.create({
+      data: {
+        name: 'Co.',
+        boroughId: borough.id,
+        type: 'Café'
+      }
+    });
+  });
+
+  afterAll(async () => {
+    /* Note:
+        - Any tables relying on Foreign Key relationships need to be deleted prior to their dependency.
+        - e.g. (Space needs to be deleted prior to Borough because rows in Space rely on a "boroughId" FK.) */
+    await prisma.space.deleteMany();
+    await prisma.borough.deleteMany();
+  });
+
+  it('returns an array of Boroughs that have at least one space associated with it;', async () => {
+    const query = gql`
+      query {
+        boroughsWithSpaces {
+          id
+          name
+        }
+      }
+    `;
+
+    const response = await testServer.executeOperation({ query });
+
+    expect(response.errors).toBeUndefined();
+    expect(response.data.boroughsWithSpaces).toBeDefined();
+    expect(response.data.boroughsWithSpaces).toHaveLength(1);
+  });
+
+  it('each Borough in the array returns an array of Spaces associated with it.', async () => {
     const query = gql`
       query {
         boroughsWithSpaces {
@@ -21,6 +66,7 @@ describe('Query: boroughsWithSpaces', () => {
 
     expect(response.errors).toBeUndefined();
     expect(response.data.boroughsWithSpaces).toBeDefined();
-    expect(response.data.boroughsWithSpaces).toHaveLength(1);
+    expect(response.data.boroughsWithSpaces[0].spaces).toBeDefined();
+    expect(response.data.boroughsWithSpaces[0].spaces).toHaveLength(1);
   });
 });
